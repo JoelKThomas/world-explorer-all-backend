cce707a59ee5d59b88b643f6c2efc0363189319b
JoelKThomas <joelkthomas11@gmail.com>
2020-09-26 14:32:05 +0530

    Basic Structure

---
+++ src/test/java/com/shadow/worldexplorer/WorldexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadow.worldexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
